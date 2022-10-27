f3ab4991e9f923d2916b01b248b934f383776e9a
Jayparkrighthere <zoeytwelve@gmail.com>
2022-10-27 17:45:38 -0600

    new unit test for delete function

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 //import org.junit.Before;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -38,6 +39,18 @@ public class CustomListTest {
         list.addCity(newCity);
         assertEquals(list.getCount(),listSize + 1);
         assertTrue(list.hasCity(newCity));
+    }
+    @Test
+    @DisplayName("Test to see if delete actually removes the city from the list")
+    void testDelete(){
+        list = MockCityList();
+        int listSize = list.getCount();
+        City newCity = new City("Estevan", "SK");
+        list.addCity(newCity);
+        assertEquals(list.getCount(),listSize + 1);
+
+        list.delete(newCity);
+        assertEquals(list.getCount(),0);
     }
 
 
